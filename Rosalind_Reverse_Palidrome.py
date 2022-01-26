58886ec7f566d320652805fa0cac051a1cf61c6c
Ethan Loo <loo.e@northeastern.edu>
2022-01-26 00:51:36 -0500

    Added sctipt for Reverse Palidrome

---
+++ Rosalind_Reverse_Palidrome.py
@@ -0,0 +1,27 @@
+from parse_fasta import parse
+seq = parse('./rosalind_revp.txt')[0]
+
+Comp_Dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+
+def rev_comp(seq=seq, cd=Comp_Dict):
+    r = ''
+    for l in seq:
+        r = cd[l.upper()]+r
+    return r
+
+print(seq)
+
+minimum = 4
+maximum = 12
+match_list = []
+for i in range(minimum, maximum+1):
+    for ind, v in enumerate(seq):
+        target=seq[ind: ind+i]
+        rc=rev_comp(seq = target, cd = Comp_Dict)
+        if len(target) >= 4 and target == rc:
+            print('match!')
+            match_list.append((ind+1, len(target)))
+
+match_list=sorted(list(set(match_list)))
+result = ''+'\n'.join(' '.join(map(str, x)) for x in match_list)
+print(result)
